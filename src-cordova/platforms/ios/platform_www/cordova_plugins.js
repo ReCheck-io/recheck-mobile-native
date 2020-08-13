f0345cafbb69ce1f4400cb1be83830d3d0423461
Byurhan Beyzat <imbyurhan@gmail.com>
2020-08-13 11:06:54 +0300

    RM/ADD platforms& rebuild for iOS

--- src-cordova/platforms/ios/platform_www/cordova_plugins.js
+++ src-cordova/platforms/ios/platform_www/cordova_plugins.js
@@ -72,10 +72,10 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
     }
   ];
   module.exports.metadata = {
-    "cordova-plugin-add-swift-support": "2.0.2",
     "cordova-plugin-dialogs": "2.0.2",
     "cordova-plugin-network-information": "2.0.2",
     "cordova-plugin-permission": "0.1.0",
+    "cordova-plugin-add-swift-support": "2.0.2",
     "cordova-plugin-qrscanner": "3.0.1",
     "cordova-plugin-splashscreen": "6.0.0",
     "cordova-plugin-statusbar": "2.4.3",
