067030965d490674566e0a4e87c7aef493d6fdb9
Byurhan Beyzat <imbyurhan@gmail.com>
2020-08-14 16:30:59 +0300

    minor updates

--- src-cordova/platforms/android/platform_www/cordova_plugins.js
+++ src-cordova/platforms/android/platform_www/cordova_plugins.js
@@ -1,6 +1,14 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
   module.exports = [
     {
+      "id": "cordova-plugin-deeplinks.universalLinks",
+      "file": "plugins/cordova-plugin-deeplinks/www/universal_links.js",
+      "pluginId": "cordova-plugin-deeplinks",
+      "clobbers": [
+        "universalLinks"
+      ]
+    },
+    {
       "id": "cordova-plugin-dialogs.notification",
       "file": "plugins/cordova-plugin-dialogs/www/notification.js",
       "pluginId": "cordova-plugin-dialogs",
@@ -14,43 +22,6 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "pluginId": "cordova-plugin-dialogs",
       "merges": [
         "navigator.notification"
-      ]
-    },
-    {
-      "id": "cordova-plugin-qrscanner.QRScanner",
-      "file": "plugins/cordova-plugin-qrscanner/www/www.min.js",
-      "pluginId": "cordova-plugin-qrscanner",
-      "clobbers": [
-        "QRScanner"
-      ]
-    },
-    {
-      "id": "cordova-plugin-permission.Permission",
-      "file": "plugins/cordova-plugin-permission/www/index.js",
-      "pluginId": "cordova-plugin-permission",
-      "clobbers": [
-        "window.plugins.Permission"
-      ]
-    },
-    {
-      "id": "cordova-plugin-permission.tests",
-      "file": "plugins/cordova-plugin-permission/tests/index.spec.js",
-      "pluginId": "cordova-plugin-permission"
-    },
-    {
-      "id": "cordova-plugin-statusbar.statusbar",
-      "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
-      "pluginId": "cordova-plugin-statusbar",
-      "clobbers": [
-        "window.StatusBar"
-      ]
-    },
-    {
-      "id": "cordova-plugin-splashscreen.SplashScreen",
-      "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
-      "pluginId": "cordova-plugin-splashscreen",
-      "clobbers": [
-        "navigator.splashscreen"
       ]
     },
     {
@@ -71,23 +42,53 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       ]
     },
     {
-      "id": "cordova-plugin-deeplinks.universalLinks",
-      "file": "plugins/cordova-plugin-deeplinks/www/universal_links.js",
-      "pluginId": "cordova-plugin-deeplinks",
+      "id": "cordova-plugin-permission.Permission",
+      "file": "plugins/cordova-plugin-permission/www/index.js",
+      "pluginId": "cordova-plugin-permission",
       "clobbers": [
-        "universalLinks"
+        "window.plugins.Permission"
+      ]
+    },
+    {
+      "id": "cordova-plugin-permission.tests",
+      "file": "plugins/cordova-plugin-permission/tests/index.spec.js",
+      "pluginId": "cordova-plugin-permission"
+    },
+    {
+      "id": "cordova-plugin-qrscanner.QRScanner",
+      "file": "plugins/cordova-plugin-qrscanner/www/www.min.js",
+      "pluginId": "cordova-plugin-qrscanner",
+      "clobbers": [
+        "QRScanner"
+      ]
+    },
+    {
+      "id": "cordova-plugin-splashscreen.SplashScreen",
+      "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+      "pluginId": "cordova-plugin-splashscreen",
+      "clobbers": [
+        "navigator.splashscreen"
+      ]
+    },
+    {
+      "id": "cordova-plugin-statusbar.statusbar",
+      "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+      "pluginId": "cordova-plugin-statusbar",
+      "clobbers": [
+        "window.StatusBar"
       ]
     }
   ];
   module.exports.metadata = {
-    "cordova-plugin-whitelist": "1.3.4",
+    "cordova-plugin-add-swift-support": "2.0.2",
+    "cordova-plugin-deeplinks": "1.1.1",
     "cordova-plugin-dialogs": "2.0.2",
-    "cordova-plugin-qrscanner": "3.0.1",
-    "cordova-plugin-permission": "0.1.0",
-    "cordova-plugin-statusbar": "2.4.3",
-    "cordova-plugin-wkwebview-engine": "1.2.1",
-    "cordova-plugin-splashscreen": "6.0.0",
     "cordova-plugin-network-information": "2.0.2",
-    "cordova-plugin-deeplinks": "1.1.1"
+    "cordova-plugin-permission": "0.1.0",
+    "cordova-plugin-qrscanner": "3.0.1",
+    "cordova-plugin-splashscreen": "6.0.0",
+    "cordova-plugin-statusbar": "2.4.3",
+    "cordova-plugin-whitelist": "1.3.4",
+    "cordova-plugin-wkwebview-engine": "1.2.1"
   };
 });
--- src-cordova/platforms/ios/platform_www/cordova_plugins.js
+++ src-cordova/platforms/ios/platform_www/cordova_plugins.js
@@ -1,6 +1,14 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
   module.exports = [
     {
+      "id": "cordova-plugin-deeplinks.universalLinks",
+      "file": "plugins/cordova-plugin-deeplinks/www/universal_links.js",
+      "pluginId": "cordova-plugin-deeplinks",
+      "clobbers": [
+        "universalLinks"
+      ]
+    },
+    {
       "id": "cordova-plugin-dialogs.notification",
       "file": "plugins/cordova-plugin-dialogs/www/notification.js",
       "pluginId": "cordova-plugin-dialogs",
@@ -9,11 +17,20 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       ]
     },
     {
-      "id": "cordova-plugin-qrscanner.QRScanner",
-      "file": "plugins/cordova-plugin-qrscanner/www/www.min.js",
-      "pluginId": "cordova-plugin-qrscanner",
+      "id": "cordova-plugin-network-information.network",
+      "file": "plugins/cordova-plugin-network-information/www/network.js",
+      "pluginId": "cordova-plugin-network-information",
       "clobbers": [
-        "QRScanner"
+        "navigator.connection",
+        "navigator.network.connection"
+      ]
+    },
+    {
+      "id": "cordova-plugin-network-information.Connection",
+      "file": "plugins/cordova-plugin-network-information/www/Connection.js",
+      "pluginId": "cordova-plugin-network-information",
+      "clobbers": [
+        "Connection"
       ]
     },
     {
@@ -28,6 +45,14 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "id": "cordova-plugin-permission.tests",
       "file": "plugins/cordova-plugin-permission/tests/index.spec.js",
       "pluginId": "cordova-plugin-permission"
+    },
+    {
+      "id": "cordova-plugin-qrscanner.QRScanner",
+      "file": "plugins/cordova-plugin-qrscanner/www/www.min.js",
+      "pluginId": "cordova-plugin-qrscanner",
+      "clobbers": [
+        "QRScanner"
+      ]
     },
     {
       "id": "cordova-plugin-statusbar.statusbar",
@@ -52,43 +77,18 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "clobbers": [
         "window.WkWebView"
       ]
-    },
-    {
-      "id": "cordova-plugin-network-information.network",
-      "file": "plugins/cordova-plugin-network-information/www/network.js",
-      "pluginId": "cordova-plugin-network-information",
-      "clobbers": [
-        "navigator.connection",
-        "navigator.network.connection"
-      ]
-    },
-    {
-      "id": "cordova-plugin-network-information.Connection",
-      "file": "plugins/cordova-plugin-network-information/www/Connection.js",
-      "pluginId": "cordova-plugin-network-information",
-      "clobbers": [
-        "Connection"
-      ]
-    },
-    {
-      "id": "cordova-plugin-deeplinks.universalLinks",
-      "file": "plugins/cordova-plugin-deeplinks/www/universal_links.js",
-      "pluginId": "cordova-plugin-deeplinks",
-      "clobbers": [
-        "universalLinks"
-      ]
     }
   ];
   module.exports.metadata = {
-    "cordova-plugin-whitelist": "1.3.4",
-    "cordova-plugin-dialogs": "2.0.2",
     "cordova-plugin-add-swift-support": "2.0.2",
-    "cordova-plugin-qrscanner": "3.0.1",
-    "cordova-plugin-permission": "0.1.0",
-    "cordova-plugin-statusbar": "2.4.3",
-    "cordova-plugin-wkwebview-engine": "1.2.1",
-    "cordova-plugin-splashscreen": "6.0.0",
+    "cordova-plugin-deeplinks": "1.1.1",
+    "cordova-plugin-dialogs": "2.0.2",
     "cordova-plugin-network-information": "2.0.2",
-    "cordova-plugin-deeplinks": "1.1.1"
+    "cordova-plugin-permission": "0.1.0",
+    "cordova-plugin-qrscanner": "3.0.1",
+    "cordova-plugin-splashscreen": "6.0.0",
+    "cordova-plugin-statusbar": "2.4.3",
+    "cordova-plugin-whitelist": "1.3.4",
+    "cordova-plugin-wkwebview-engine": "1.2.1"
   };
 });
